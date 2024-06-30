20ada5610941c963dfb0864c170bf2791f5552f7
EmelineLeduc <leduc.emeline44@gmail.com>
2024-06-30 14:42:25 +0200

    add logic to display bloomers according to a condition (on next month)

--- front/src/LeavingArrivingBloomers/index.tsx
+++ front/src/LeavingArrivingBloomers/index.tsx
@@ -7,10 +7,27 @@ interface LeavingArrivingBloomers {
   leavingBloomers: MissionByDate;
 }
 
+const filterBloomersByDate = (bloomers: MissionByDate) => {
+  const today = new Date();
+  const endOfNextMonth = new Date(today.getFullYear(), today.getMonth() + 2, 0);
+
+  const filteredEntries = Object.entries(bloomers).filter((date) => {
+    const dateMission = new Date(date[0]);
+    if (dateMission >= today && dateMission <= endOfNextMonth) {
+      return date;
+    }
+  });
+  const result = Object.fromEntries(filteredEntries);
+  return result;
+};
+
 const LeavingArrivingBloomers = ({
   arrivingBloomers,
   leavingBloomers,
 }: LeavingArrivingBloomers) => {
+  const filteredBloomersByArrivingDate = filterBloomersByDate(arrivingBloomers);
+  const filteredBloomersByLeavingDate = filterBloomersByDate(leavingBloomers);
+
   return (
     <div className="centerWrapper">
       <div className="container">
@@ -18,11 +35,11 @@ const LeavingArrivingBloomers = ({
         <div className="scrollContent">
           <div className="bloomerListContainer">
             <BloomerDateList
-              bloomerDateList={arrivingBloomers}
+              bloomerDateList={filteredBloomersByArrivingDate}
               stateBloomer="arriving"
             />
             <BloomerDateList
-              bloomerDateList={leavingBloomers}
+              bloomerDateList={filteredBloomersByLeavingDate}
               stateBloomer="leaving"
             />
           </div>
