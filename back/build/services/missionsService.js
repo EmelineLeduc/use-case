2634cbec6c336663a43e49de7dff9fd6df37c715
EmelineLeduc <leduc.emeline44@gmail.com>
2024-07-01 19:21:06 +0200

    update missionsService data

--- back/build/services/missionsService.js
+++ back/build/services/missionsService.js
@@ -5,8 +5,8 @@ const getMissions = () => [
   {
     id: '1',
     label: "Développement d'une application mobile",
-    beginDate: '2024-01-01',
-    endDate: '2024-09_01',
+    beginDate: '2024-07-08',
+    endDate: '2024-07-29',
     missionType: 'Développement',
     freelance: {
       id: 'f1',
@@ -18,14 +18,40 @@ const getMissions = () => [
   {
     id: '2',
     label: 'Audit de sécurité informatique',
-    beginDate: '2023-02-15',
-    endDate: '2023-03-15',
+    beginDate: '2024-07-08',
+    endDate: '2024-07-30',
     missionType: 'Audit',
     freelance: {
       id: 'f2',
       firstname: 'Marie',
       lastname: 'Durand',
       email: 'marie.durand@example.com',
+    },
+  },
+  {
+    id: '3',
+    label: "Développement d'une application web",
+    beginDate: '2024-07-12',
+    endDate: '2024-07-30',
+    missionType: 'Développement',
+    freelance: {
+      id: 'f3',
+      firstname: 'Michel',
+      lastname: 'Cacahuete',
+      email: 'michel.cacahuete@example.com',
+    },
+  },
+  {
+    id: '4',
+    label: 'Audit',
+    beginDate: '2025-07-12',
+    endDate: '2025-08-12',
+    missionType: 'Audit',
+    freelance: {
+      id: 'f4',
+      firstname: 'Sandy',
+      lastname: 'Courgette',
+      email: 'sandy.courgette@example.com',
     },
   },
 ];
